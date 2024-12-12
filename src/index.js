4aaef51830c9cc0c0d8c8fd4f6c84b9c0f273779
Deco <decoin1987@gmail.com>
2024-12-12 06:31:19 +0500

    ff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.WebApp
-console.log(tg.ready())
-if (tg.ready()) {
+console.log(tg.ready)
+if (tg.ready) {
     console.log('ready')
     const params = new URLSearchParams(window.location.search);
 
